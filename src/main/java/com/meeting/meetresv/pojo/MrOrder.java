5d35a44b34c2810b5d15be845b9b2e94b6b31d5f
Sarp <1176648843@qq.com>
2019-01-04 17:59:04 +0800

    修复ie浏览器日期显示NaN

--- src/main/java/com/meeting/meetresv/pojo/MrOrder.java
+++ src/main/java/com/meeting/meetresv/pojo/MrOrder.java
@@ -1,5 +1,6 @@
 package com.meeting.meetresv.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
@@ -41,11 +42,23 @@ public class MrOrder {
         this.user = user == null ? null : user.trim();
     }
 
-    //使用此参数将string转换为date
+    /*
+        以下日期形式兼容各种浏览器
+        var d = new Date(2011, 01, 07); // yyyy, mm-1, dd
+        var d = new Date(2011, 01, 07, 11, 05, 00); // yyyy, mm-1, dd, hh, mm, ss
+        var d = new Date("02/07/2011"); // "mm/dd/yyyy"
+        var d = new Date("02/07/2011 11:05:00"); // "mm/dd/yyyy hh:mm:ss"
+        var d = new Date(1297076700000); // milliseconds
+        var d = new Date("Mon Feb 07 2011 11:05:00 GMT"); // ""Day Mon dd yyyy hh:mm:ss GMT/UTC
+    */
+
+    //此注解将string参数绑定到date
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern="yyyy/MM/dd",timezone="GMT+8")//返回时转为json
     public Date getDay() {
         return day;
     }
+
 
     public void setDay(Date day) {
         this.day=day;
